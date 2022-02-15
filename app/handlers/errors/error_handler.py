f8e2ed97170c9693e9544f0db6530e8a0bb701b3
abanshikov <abanshikov@yandex.ru>
2022-02-15 14:47:25 +0700

    Изменил файл логирования

--- app/handlers/errors/error_handler.py
+++ app/handlers/errors/error_handler.py
@@ -1,5 +1,4 @@
 import logging
-from utils.misc.logger import log
 from aiogram.utils.exceptions import (TelegramAPIError,
                                       MessageNotModified,
                                       CantParseEntities)
@@ -18,18 +17,18 @@ async def errors_handler(update, exception):
 
     if isinstance(exception, MessageNotModified):
         # do something here?
-        log.error('Message is not modified')
+        logging.error('Message is not modified')
         return True
 
     if isinstance(exception, CantParseEntities):
         # or here
-        log.error(f'CantParseEntities: {exception} \nUpdate: {update}')
+        logging.error(f'CantParseEntities: {exception} \nUpdate: {update}')
         return True
 
     #  MUST BE THE  LAST CONDITION (ЭТО УСЛОВИЕ ВСЕГДА ДОЛЖНО БЫТЬ В КОНЦЕ)
     if isinstance(exception, TelegramAPIError):
-        log.error(f'TelegramAPIError: {exception} \nUpdate: {update}')
+        logging.error(f'TelegramAPIError: {exception} \nUpdate: {update}')
         return True
 
     # At least you have tried.
-    log.error(f'Update: {update} \n{exception}')
+    logging.error(f'Update: {update} \n{exception}')
--- app/utils/misc/logging.py
+++ app/utils/misc/logging.py
@@ -1,5 +1,22 @@
 import logging
 
-logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] '
-                           u'#%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.INFO, )
+
+SAVE_FILE = False
+FILE_NAME = "history.log"
+CONSOLE_LEVEL = logging.INFO
+FILE_LEVEL = logging.WARNING
+
+
+if SAVE_FILE:
+    logging.basicConfig(
+        filename=FILE_NAME,
+        format=u'%(levelname)s:\t[%(filename)s] [LINE:%(lineno)s] [%(asctime)s]:\t%(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=FILE_LEVEL,
+    )
+else:
+    logging.basicConfig(
+        format=u'%(levelname)s:\t[%(filename)s] [LINE:%(lineno)s] [%(asctime)s]:\t%(message)s',
+        datefmt='%H:%M',
+        level=CONSOLE_LEVEL,
+    )
